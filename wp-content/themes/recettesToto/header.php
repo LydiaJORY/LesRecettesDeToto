4d7c985b52a888cbe1f305f8bf1e4325ca0fe1c5
Lylyy <jory.lydia@gmail.com>
2015-12-17 23:11:04 +0100

    new sidebar (still in progress)

--- wp-content/themes/recettesToto/header.php
+++ wp-content/themes/recettesToto/header.php
@@ -20,6 +20,8 @@
 	<![endif]-->
 	<link href="<?php echo esc_url( get_template_directory_uri() ); ?>/css/bootstrap.min.css" rel="stylesheet">
     <link href="<?php echo esc_url( get_template_directory_uri() ); ?>/css/style.css" rel="stylesheet">
+    <link href="<?php echo esc_url( get_template_directory_uri() ); ?>/css/homepage.css" rel="stylesheet">
+    <link href="<?php echo esc_url( get_template_directory_uri() ); ?>/css/sidebar.css" rel="stylesheet">
 </head>
 <body <?php body_class(); ?>>
 	<div class="main">
--- wp-content/themes/recettesToto/sidebar.php
+++ wp-content/themes/recettesToto/sidebar.php
@@ -1,4 +1,49 @@
 <aside class="menu">     
-    <a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/logo.png" alt="<?php bloginfo( 'name' ); ?>" /></a>
+    <div class="logoWrap">	
+
+    	<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
+    		<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/logo.png" alt="<?php bloginfo( 'name' ); ?>" class="logo" />
+    	</a>
+    </div>
+
+    <div class="menu__rubriques">
+	    
+	    <div class="menu__recettesSalees">	
+	    	
+	    	<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/recettessaleesPicto.png" alt="recettes salees" title="" class="recettesSaleesIcone">
+
+	    	<p class="recettesSaleesTitre">Recettes Salées</p>
+	    </div>
+
+	    <div class="menu__recettesSalees">	
+	    	
+	    	<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/recettessucreePicto.png" alt="recettes salees" title="" class="recettesSaleesIcone">
+
+	    	<p class="recettesSaleesTitre">Recettes Sucrées</p>
+	    </div>
+
+	    <div class="menu__recettesSalees">	
+	    	
+	    	<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/ustencilesPicto.png" alt="recettes salees" title="" class="recettesSaleesIcone">
+
+	    	<p class="recettesSaleesTitre">Ustenciles</p>
+	    </div>
+
+
+	    <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/aproposPicto.png" alt="recettes salees" title="" class="aproposPicto">
+	    <p class="aproposTitre">
+	    	A Propos
+	    </p>
+	    
+	    <p class="aproposDetail">
+	    	Les recettes de Toto est un recceuil de recettes salées, 
+	    	sucrées et de conseils de cuisine en tout genre. Un fourre tout, en somme :)
+	    </p>
+	    
+
+
+	    Rechercher
+	</div>
+
     <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/menu.png" alt="" />
 </aside>
